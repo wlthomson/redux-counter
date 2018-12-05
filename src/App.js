1e479ab071850e8aac1b05ad5ed5e09a18428a21
wlthomson <will.tho@outlook.com>
2018-12-05 22:10:56 +1300

    Add basic counter component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Counter from './Counter';
 
-class Counter extends Component {
-  state = { count: 0 }
-
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-
-  decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  }
-
+export default class App extends Component {
   render() {
     return (
       <div>
-        <h2>Counter</h2>
-        <div>
-          <button onClick={this.decrement}>-</button>
-          <span>{this.state.count}</span>
-          <button onClick={this.increment}>+</button>
-        </div>
+        <Counter />
       </div>
     );
   }
 }
-
-export default Counter;
---
+++ src/Counter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
