df4ec791ac9d1e5e69762f7b9a161361004ca5c0
wlthomson <will.tho@outlook.com>
2018-12-10 23:17:32 +1300

    Add actions to reducer

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import { createStore } from 'redux';
 
 import Counter from './Counter';
 
-const store = createStore(() => {
-  return { count: 42 };
-});
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
